e9b36f9426afd1a4d94282b2ac3697fecbd777d2
xya499 <302870488@qq.com>
2022-12-23 16:53:18 +0800

    init Project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from "./common/database/database.module";
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
